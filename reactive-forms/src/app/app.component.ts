e5959042739b67d2e9769b37d2555e2ca6d1104e
Anairoz <zoriana.dnytryk@gmail.com>
2019-03-30 20:03:53 +0200

    Update

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms';
+ registrationForm = new FormGroup({
+     userName: new FormControl('Zoriana'),
+     password: new FormControl(''),
+     confirmPassword: new FormControl(''),
+     address: new FormGroup({
+         city: new FormControl('New York'),
+         state: new FormControl('New York'),
+         postalCode: new FormControl('')
+     })
+ });
+ loadApiData(){
+     this.registrationForm.patchValue({
+             userName: 'Dean',
+             password: '1234',
+             confirmPassword: '1234'
+     })
+ }
 }
